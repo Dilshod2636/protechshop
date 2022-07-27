5ba8437287a876ae4faac9c2941f697f93e4112b
Dilshodbek <bohodirovdilshod21@gmail.com>
2022-07-27 15:28:19 +0300

    js

---
+++ main.js
@@ -0,0 +1,11 @@
+let xd1 = document.getElementById("xd");
+let xd3 = document.getElementById("xd2");
+
+function laptop_teyi1(){
+    xd1.style.display = "flex";
+    xd3.style.color = "#067ad4";
+}
+
+function laptop_teyi2(){
+    xd1.style.display = "none";
+}
