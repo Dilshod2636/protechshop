8143e5593627613db204085ee15345e4e2d60cb2
Dilshodbek <bohodirovdilshod21@gmail.com>
2022-07-28 13:54:02 +0300

    footer

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let xd1 = document.getElementById("xd");
-let xd3 = document.getElementById("xd2");
+let mas = document.getElementsByClassName("laptop_teyi");
+let rang = document.querySelectorAll('.buy a');
 
-function laptop_teyi1(){
-    xd1.style.display = "flex";
-    xd3.style.color = "#067ad4";
+function laptop_teyi1(a){
+    mas[a-1].style.display = "flex";
+    rang[a-1].style.backgroundColor = "#067ad4"
 }
 
-function laptop_teyi2(){
-    xd1.style.display = "none";
+function laptop_teyi2(a){
+    mas[a-1].style.display = "none";
+    rang[a-1].style.backgroundColor = "#aaa";
 }
+
